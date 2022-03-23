87bf7d5898adaf09c69b53be69fadd3e53c2f735
Sriram Ramachandran <sriram47@gmail.com>
2022-03-23 11:10:18 -0700

    Add humidity data point

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (address1, address2, callback) => {
         } else if (body.error) {
             callback('Unable to determine the location. Please try again.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.'
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The current humidity is '+ body.current.humidity +'%.'
                 // currentTemp: response.body.current.temperature,
                 // currentFeelsLike: response.body.current.feelslike,
                 // weatherDesc: response.body.current.weather_descriptions[0]
